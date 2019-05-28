ddd37e09ca1b0a6720a2d4800ac6143a0edcc7e9
lee <licc5@asiainfo.com>
2019-05-28 17:13:37 +0800

    Initial commit

---
+++ src/club/dafty/jvm/classloader/ClassLoaderDemo.java
@@ -0,0 +1,12 @@
+package club.dafty.jvm.classloader;
+
+/**
+ * @author leechengchao@foxmail.com
+ * @version 1.0
+ * @date 2019/5/28 17:02
+ */
+public class ClassLoaderDemo {
+    public static void main(String[] args) {
+        Object o = new Object();
+    }
+}
